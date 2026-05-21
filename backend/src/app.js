270032a04cbc3c2cb86026e7806c286a62776010
karanRawat <rawatkaran0017@gmail.com>
2026-05-21 12:53:44 +0530

    backend link setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,6 +18,9 @@ app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
