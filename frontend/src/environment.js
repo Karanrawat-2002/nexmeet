f0e15a2643c012cc082cb3df2e14a091da89703b
karanRawat <rawatkaran0017@gmail.com>
2026-05-23 08:34:00 +0530

    changing link

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,5 @@
-// const server = process.env.NODE_ENV === "production"
-//     ? process.env.BACKEND_URL
-//     : "http://localhost:8000";
+const server = process.env.NODE_ENV === "production"
+    ? process.env.BACKEND_URL
+    : "http://localhost:8000";
 
-const server = "https://nexmeet-1-k99o.onrender.com/";
 export default server;
